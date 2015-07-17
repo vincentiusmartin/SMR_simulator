e31b2d2e795a9a0967130af6cef4e4f0f4951d59
Vincentius Martin <vincentiusmartin@gmail.com>
2015-07-17 17:31:38 +0700

    provisional handling for corner case

--- smr.py
+++ smr.py
@@ -28,22 +28,24 @@ from tqdm import *
 SECTOR_SIZE = 512 #default 512B
 
 #DISK_SIZE = 9437184 / SECTOR_SIZE #default 1TB-1099511627776
-PCACHE_SIZE = 25600 / SECTOR_SIZE #cache in this script tantamounts to persistent_cache, default 100GB-107374182400
+PCACHE_SIZE = 25600 / SECTOR_SIZE #pcache in this script is tantamount to persistent_cache, default 100GB-107374182400
 BAND_SIZE = 5120 / SECTOR_SIZE #default 10MB-10485760
 
 # Disk element
 
 current_pcache_idx = 0
-
 pcache_map = []
 
 # Input file
 
-trace = open('/home/vincentius/Documents/score-lab/other/io-shelter/T3/DisplayAdsPayload/Combined/mytest.txt','r');
+trace = open('in/trace.txt','r');
 
 #===============================================================================================
 
 # Functions
+
+class HaltException(Exception):
+    pass
 
 def cleanPCache(time,devno):
     
@@ -53,7 +55,7 @@ def cleanPCache(time,devno):
     for blkno,blkcount in pcache_map:
         starting_band = int(blkno / BAND_SIZE)
         band_count = int(math.ceil((blkcount + (blkno % BAND_SIZE)) / BAND_SIZE))
-        print(str(starting_band) + "&" + str(band_count) + " <-bandcount, blkno&blkcount-> " + str(blkno) + "&" + str(blkcount))
+        #print(str(starting_band) + "&" + str(band_count) + " <-bandcount, blkno&blkcount-> " + str(blkno) + "&" + str(blkcount))
         for i in range (starting_band, starting_band + band_count):
             dirty_band.append(i)
             
@@ -67,11 +69,15 @@ def cleanPCache(time,devno):
     #clear pcache
     current_pcache_idx = 0
     del pcache_map[:]
-    print("endclean")
+    #print("endclean")
         
 def handleWrite(time, devno, blkno, blkcount):
 
     global current_pcache_idx
+    
+    #TODO: assign better handle for over-limit case
+    if (current_pcache_idx + blkcount > PCACHE_SIZE):
+        raise HaltException("write size is larger than persistent cache limit! script terminated")
     
     #write to persistent cache
     sys.stdout.write("{} {} {} {} {}\n".format(time, devno, current_pcache_idx, blkcount, 0))
