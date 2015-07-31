327f351d309a070849ee6dc1ade5756d8e832c09
Vincentius Martin <vincentiusmartin@gmail.com>
2015-07-31 13:47:17 +0700

    modified

--- smr.py
+++ smr.py
@@ -73,7 +73,7 @@ class HaltException(Exception):
     pass
 
 def cleanPCache(time,devno):
-    result.write("startclean")
+    #result.write("startclean")
     global current_pcache_idx
     global numberOfClean
     global totalDirtyBands
@@ -107,7 +107,7 @@ def cleanPCache(time,devno):
     #clear pcache
     current_pcache_idx = 0
     del pcache_map[:]
-    result.write("endclean")
+    #result.write("endclean")
         
 def handleWrite(time, devno, blkno, blkcount):
 
