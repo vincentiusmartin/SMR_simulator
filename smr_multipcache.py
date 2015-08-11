20548d6f6236962bef4d8fa5a5c2e39bdfeb7dc3
Vincentius Martin <vincentiusmartin@gmail.com>
2015-08-11 16:13:05 +0700

    metrics

--- smr_multipcache.py
+++ smr_multipcache.py
@@ -52,8 +52,8 @@ TOTAL_PCACHE = args.pctotal / SECTOR_SIZE #default 10GB - 10737418240
 
 # Variables
 diskset_size = DISK_SIZE // (TOTAL_PCACHE // PCACHE_UNIT) #give temporary value first
-band_count = (diskset_size - PCACHE_UNIT) // BAND_SIZE
-diskset_size = BAND_SIZE * band_count + PCACHE_UNIT
+band_unit = (diskset_size - PCACHE_UNIT) // BAND_SIZE
+diskset_size = BAND_SIZE * band_unit + PCACHE_UNIT
 
 #diskset_size = BAND_SIZE * band_count + PCACHE_UNIT
 #need to compute persistent cache for remainder case
@@ -89,18 +89,25 @@ def cleanPCache(time,devno):
     result.write("start cleanup\n")
     
     global current_pcache_idx
+    global numberOfClean
+    global totalDirtyBands
 
     dirty_band = set()
+    
+    #METRICS part - increment number of clean
+    numberOfClean += 1
     
     for blkno,blkcount in pcache_map:
         starting_band = int(blkno // BAND_SIZE)
         band_count = int(math.ceil((blkcount + (blkno % BAND_SIZE)) / BAND_SIZE))
         
         for i in range (starting_band, starting_band + band_count):
-            dirty_band.add(i)
+            dirty_band.add(i)    
+            #METRICS part - total dirty band, used for average dirty bands per clean
+            totalDirtyBands += 1  
             
     for band in sorted(dirty_band):
-        starting_blkno = band * BAND_SIZE + ((band * BAND_SIZE) // (BAND_SIZE * band_count) + 1) * PCACHE_UNIT
+        starting_blkno = band * BAND_SIZE + ((band * BAND_SIZE) // (BAND_SIZE * band_unit) + 1) * PCACHE_UNIT
         if result_cleanup is None:
             #read
             result.write("{} {} {} {} {}\n".format(time, devno, starting_blkno, BAND_SIZE, 1))
@@ -119,10 +126,13 @@ def cleanPCache(time,devno):
     result.write("end cleanup\n")
 
 def handleRead(time, devno, blkno, blkcount):
-    #print "\n" + PCACHE_RATIO + " -- " + str(ratio)
+    global totalRead
+    
+    #METRICS part - increment total read
+    totalRead += 1
     
     # count starting blkno
-    blkno += (blkno // (BAND_SIZE * band_count) + 1) * PCACHE_UNIT
+    blkno += (blkno // (BAND_SIZE * band_unit) + 1) * PCACHE_UNIT
     while blkcount > 0:
         #avoid pcache if needed
         if blkno % (diskset_size) == 0:
@@ -143,6 +153,12 @@ def handleRead(time, devno, blkno, blkcount):
         
 def handleWrite(time, devno, blkno, blkcount):
     global current_pcache_idx
+    global writesPutInPCache
+    global sectorsPutInPCache
+
+    #METRICS part - writes and sectors put in persistent cache
+    writesPutInPCache += 1
+    sectorsPutInPCache += blkcount
 
     #TODO: might need to create better handling for over-limit case
     if (current_pcache_idx + blkcount > TOTAL_PCACHE):
@@ -167,11 +183,21 @@ def handleWrite(time, devno, blkno, blkcount):
         
 def printConfiguration():
     print("------------Configuration------------")
-    print("Persistent cache size: " + str(TOTAL_PCACHE))
-    print("Band size: " + str(BAND_SIZE))
-    print("Bands that follow a persistent cache unit: " + str(band_count))
+    print("Persistent cache size: " + str(TOTAL_PCACHE)) + " sectors"
+    print("Band size: " + str(BAND_SIZE)) + " sectors"
+    print("Bands that follow a persistent cache unit: " + str(band_unit))
     print("Total shelters: " + str(DISK_SIZE / diskset_size))
     print("-------------------------------------")
+
+def printSummary():
+    print("------------Result Summary------------")
+    print("Number of clean: " + str(numberOfClean))
+    print("Total writes to persistent cache: " + str(writesPutInPCache))
+    print("Total read to disk: " + str(totalRead))
+    print("Total sectors to persistent cache: " + str(sectorsPutInPCache))
+    if numberOfClean > 0:
+        print("Averages dirty bands per clean: " + str(float(totalDirtyBands) / numberOfClean));
+    print("--------------------------------------")
 
 #===============================================================================================
 
@@ -188,12 +214,10 @@ if __name__ == "__main__":
         
         if flag == '1': #read
             handleRead(time, devno, blkno, blkcount)
-            #METRICS part - read to disk
-            totalRead += 1
         else: #write
             handleWrite(time, devno, blkno, blkcount)
         
     result.close()
-    #printSummary()
+    printSummary()
         
 
